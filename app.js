4e330c4c67bcfd666aaf2cc5a0b183292cff48d7
hoz-code <93412555+hoz-code@users.noreply.github.com>
2024-08-23 15:54:35 -0500

    Update app.js
    Change line 15, and it was commented, to include the new line 16 with different URL

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const pool = new Pool({
 });
 
 // Bored API base URL
-const BORED_API_BASE_URL = 'https://www.boredapi.com/api/';
+//const BORED_API_BASE_URL = 'https://www.boredapi.com/api/';
+const BORED_API_BASE_URL = 'https://bored-api.appbrewery.com/random';
 
 async function getRandomActivity() {
   try {
